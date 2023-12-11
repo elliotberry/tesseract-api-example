8c220300b656d2db9706bb2484fa65081941958a
elliot b =^._.^= ∫ <elliot@email.gd>
2023-12-11 08:20:13 -0500

    adding fluff

--- index.js
+++ index.js
@@ -1,43 +1,64 @@
-import Fastify from 'fastify';
-import multipart from 'fastify-multipart';
-import { createWorker } from 'tesseract.js';
+import Fastify from 'fastify'
+import multipart from 'fastify-multipart'
+import { createWorker } from 'tesseract.js'
+import auth from '@fastify/basic-auth';
+import 'dotenv/config'
+const authenticate = {realm: 'Webapp'}
+const fastify = Fastify()
 
-const fastify = Fastify();
+fastify.register(multipart)
 
-fastify.register(multipart);
+fastify.register(auth, { validate, authenticate })
+// `this` inside validate is `fastify`
+function validate (username, password, req, reply, done) {
+  if (username === `${process.env.USERNAME}` && password === `${process.env.PASSWORD}`) {
+    done()
+  } else {
+    done(new Error('no'))
+  }
+}
 
+fastify.after(() => {
+  fastify.addHook('onRequest', fastify.basicAuth)
+
+  fastify.get('/', (req, reply) => {
+    reply.send({ hello: 'world' })
+  })
+})
 const getText = async (buffer) => {
-    const worker = await createWorker('eng');
-    await worker.load(buffer);
-    await worker.loadLanguage('eng');
-    await worker.initialize('eng');
+    const worker = await createWorker('eng')
+    await worker.load(buffer)
+    await worker.loadLanguage('eng')
+    await worker.initialize('eng')
 
-    const { data: { text } } = await worker.recognize(buffer);
-    await worker.terminate();
-    return text;
+    const {
+        data: { text },
+    } = await worker.recognize(buffer)
+    await worker.terminate()
+    return text
 }
 
 fastify.post('/upload', async (request, reply) => {
-    const data = await request.file();
-    const buffer = await data.toBuffer();
+    const data = await request.file()
+    const buffer = await data.toBuffer()
 
-   let now = new Date();
-    let text = await getText(buffer);
- 
-    let then = new Date();
-    console.log(then - now);
+    let now = new Date()
+    let text = await getText(buffer)
 
-    reply.send({ text });
-});
+    let then = new Date()
+    console.log(then - now)
+
+    reply.send({ text })
+})
 
 const start = async () => {
     try {
-        await fastify.listen(3000);
-        console.log(`Server listening on ${fastify.server.address().port}`);
+        await fastify.listen(3000)
+        console.log(`Server listening on ${fastify.server.address().port}`)
     } catch (error) {
-        fastify.log.error(error);
-        process.exit(1);
+        fastify.log.error(error)
+        process.exit(1)
     }
-};
+}
 
-start();
+start()
